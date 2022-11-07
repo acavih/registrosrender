2e9a7e3d9937e1cfd5bd9e8dccf2622c09b214c4
acavih <hola@acavih.org>
2022-11-07 11:43:47 +0100

    Actualizar baseUrl

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,7 +68,7 @@ const config = {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: isProd ? '/api/v1' : 'http://localhost:8080/api'
+    baseURL: isProd ? '/api' : 'http://localhost:8080/api'
   },
 
   auth: {
