9e6c2254a47d8d678fb2dad9ff04d7b4a543b907
acavih <hola@acavih.org>
2022-11-08 11:52:42 +0100

    Bocetar componente autocomplete

---
+++ nuxt-app/components/books/BookEditor.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <text-area-block />
+  </div>
+</template>
+
+<script>
+import TextAreaBlock from "./TextAreaBlock.vue";
+export default {
+  components: { TextAreaBlock },
+};
+</script>
+
+<style>
+</style>
---
+++ nuxt-app/pages/admin/exper.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-sheet>
+    <v-card>
+      <v-card-title> Experimentos </v-card-title>
+      <v-card-text>
+        <book-editor />
+      </v-card-text>
+    </v-card>
+  </v-sheet>
+</template>
+
+<script>
+import BookEditor from "@/components/books/BookEditor.vue";
+
+export default {
+  components: { BookEditor },
+  name: "ExperimentsVue",
+  layout: "guest",
+  head() {
+    return {
+      title: "Experimentos",
+    };
+  },
+  mounted() {},
+};
+</script>
