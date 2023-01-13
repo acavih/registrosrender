cc8edea0b82e8fa15bbb0645302a0084979658fc
Acavih <registrosacavih@gmail.com>
2023-01-13 11:38:39 +0100

    Limpiar store y reseterar partners module

--- nuxt-app/store/partners.js
+++ nuxt-app/store/partners.js
@@ -1,45 +1,23 @@
 export const state = () => ({
-  partners: [],
-  totalItems: 0
+  partnersList: []
 })
 
+/**
+* @type {import('vuex').GetterTree<ReturnType<typeof state>>, any>}
+*/
 export const getters = {
 }
 
+/**
+* @type {import('vuex').MutationTree<ReturnType<typeof state>>>}
+*/
 export const mutations = {
-  setPartners: (state, { partners, totalItems }) => {
-    state.partners = partners
-    state.totalItems = totalItems
-  },
-  addPartner: (state, partner) => {
-    state.partners.push(partner)
-  },
-  replacePartner: (state, partner) => {
-    state.partners = [...state.partners.map(p => {
-      if (p._id === partner._id) return partner
-      return p
-    })]
-  }
 }
 
+/**
+* @type {import('vuex').ActionTree<ReturnType<typeof state>>, any>}
+*/
 export const actions = {
-  async retrievePartners({ commit }, query) {
-    const resources = await this.$axios.get('/partners', {
-      params: query
-    })
-    commit('setPartners', resources.data.payload)
-  },
-  async retrievePartner({ commit }, id) {
-    const resources = await this.$axios.get('/partners/' + id)
-    commit('addPartner', resources.data.payload)
-  },
-  async addPartner(_ctx, partnerData) {
-    await this.$axios.post('/partners', partnerData)
-  },
-  async updatePartner({ commit }, partnerData) {
-    await this.$axios.put('/partners/' + partnerData._id, partnerData)
-    commit('replacePartner', partnerData)
-  }
 }
 
 export const strict = false
