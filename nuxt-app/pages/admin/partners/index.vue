98b888beb198cd12bc5d7632ada982d1dd129bde
Acavih <registrosacavih@gmail.com>
2023-01-13 11:37:32 +0100

    Iniciar página de socios básica

---
+++ nuxt-app/pages/admin/partners/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-sheet>
+    <v-card>
+      <v-card-title>Socios</v-card-title>
+      <v-card-text>
+        contenido
+      </v-card-text>
+    </v-card>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+
+}
+</script>
