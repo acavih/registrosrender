c2b758a4bc3dac05eeee88e90f06fe2ab8806198
Acavih <registrosacavih@gmail.com>
2023-01-13 11:29:25 +0100

    script dev:backend

--- nuxt.config.js
+++ nuxt.config.js
@@ -72,7 +72,7 @@ const config = {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/api'
+    baseURL: 'http://localhost:8080/api'
   },
 
   auth: {
